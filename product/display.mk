c991e1ba661cb78494f1883d4da443a6105c466d
Marek8299 <marek8299@outlook.com>
2024-08-08 20:26:35 +0200

    display: use threaded render engine

--- product/display.mk
+++ product/display.mk
@@ -4,4 +4,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_gl_backpressure=1 \
     debug.sf.latch_unsignaled=1 \
     persist.hwc.mdpcomp.enable=true \
-    ro.sf.lcd_density=320
+    ro.sf.lcd_density=320 \
+    debug.renderengine.backend=threaded
